582b7a2c3a0f48efe082329050095766a5ff8c24
Alexander Weinrauch <alexander.weinrauch@tugraz.at>
2024-03-20 11:42:47 +0100

    Increased display timeout and fixed per device config (breaking change)

--- hypr/scripts/sleep.sh
+++ hypr/scripts/sleep.sh
@@ -1,5 +1,5 @@
 swayidle -w \
-        timeout 120 'swaylock -f' \
-        timeout 125 'hyprctl dispatch dpms off' \
+        timeout 1200 'swaylock -f' \
+        timeout 1205 'hyprctl dispatch dpms off' \
         resume 'hyprctl dispatch dpms on'
 
